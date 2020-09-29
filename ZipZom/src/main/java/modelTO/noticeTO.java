820028d355abf1d714745f2b0ed890c34af716ff
Hoon <Hoon@DESKTOP-R71U3UU>
2020-09-29 17:32:28 +0900

    TO 수정

---
+++ ZipZom/src/main/java/modelTO/noticeTO.java
@@ -0,0 +1,55 @@
+package modelTO;
+
+public class noticeTO {
+	private int SEQ_N;
+	private String SUBJECT;
+	private String WRITER;
+	private String CONTENT;
+	private String FILENAME;
+	private int HIT;
+	private String DATE;
+	
+	public int getSEQ_N() {
+		return SEQ_N;
+	}
+	public void setSEQ_N(int sEQ_N) {
+		SEQ_N = sEQ_N;
+	}
+	public String getSUBJECT() {
+		return SUBJECT;
+	}
+	public void setSUBJECT(String sUBJECT) {
+		SUBJECT = sUBJECT;
+	}
+	public String getWRITER() {
+		return WRITER;
+	}
+	public void setWRITER(String wRITER) {
+		WRITER = wRITER;
+	}
+	public String getCONTENT() {
+		return CONTENT;
+	}
+	public void setCONTENT(String cONTENT) {
+		CONTENT = cONTENT;
+	}
+	public String getFILENAME() {
+		return FILENAME;
+	}
+	public void setFILENAME(String fILENAME) {
+		FILENAME = fILENAME;
+	}
+	public int getHIT() {
+		return HIT;
+	}
+	public void setHIT(int hIT) {
+		HIT = hIT;
+	}
+	public String getDATE() {
+		return DATE;
+	}
+	public void setDATE(String dATE) {
+		DATE = dATE;
+	}
+	
+}
---
+++ ZipZom/src/main/java/modelTO/option_customerTO.java
@@ -0,0 +1,103 @@
+package modelTO;
+
+public class option_customerTO {
+	private int SEQ_OC;
+	private int PSEQ_OC;
+	private boolean BED;
+	private boolean WASHING_MACHINE;
+	private boolean DESC;
+	private boolean DRYING_MACHINE;
+	private boolean INDUCTION;
+	private boolean ELECTRIC_RANGE;
+	private boolean GAS_RANGE;
+	private boolean WORDROBE;
+	private boolean SINK;
+	private boolean SHOE_RACK;
+	private boolean REFRIGERATOR;
+	private boolean BATH_TUBE;
+	
+	public int getSEQ_OC() {
+		return SEQ_OC;
+	}
+	public void setSEQ_OC(int sEQ_OC) {
+		SEQ_OC = sEQ_OC;
+	}
+	public int getPSEQ_OC() {
+		return PSEQ_OC;
+	}
+	public void setPSEQ_OC(int pSEQ_OC) {
+		PSEQ_OC = pSEQ_OC;
+	}
+	public boolean isBED() {
+		return BED;
+	}
+	public void setBED(boolean bED) {
+		BED = bED;
+	}
+	public boolean isWASHING_MACHINE() {
+		return WASHING_MACHINE;
+	}
+	public void setWASHING_MACHINE(boolean wASHING_MACHINE) {
+		WASHING_MACHINE = wASHING_MACHINE;
+	}
+	public boolean isDESC() {
+		return DESC;
+	}
+	public void setDESC(boolean dESC) {
+		DESC = dESC;
+	}
+	public boolean isDRYING_MACHINE() {
+		return DRYING_MACHINE;
+	}
+	public void setDRYING_MACHINE(boolean dRYING_MACHINE) {
+		DRYING_MACHINE = dRYING_MACHINE;
+	}
+	public boolean isINDUCTION() {
+		return INDUCTION;
+	}
+	public void setINDUCTION(boolean iNDUCTION) {
+		INDUCTION = iNDUCTION;
+	}
+	public boolean isELECTRIC_RANGE() {
+		return ELECTRIC_RANGE;
+	}
+	public void setELECTRIC_RANGE(boolean eLECTRIC_RANGE) {
+		ELECTRIC_RANGE = eLECTRIC_RANGE;
+	}
+	public boolean isGAS_RANGE() {
+		return GAS_RANGE;
+	}
+	public void setGAS_RANGE(boolean gAS_RANGE) {
+		GAS_RANGE = gAS_RANGE;
+	}
+	public boolean isWORDROBE() {
+		return WORDROBE;
+	}
+	public void setWORDROBE(boolean wORDROBE) {
+		WORDROBE = wORDROBE;
+	}
+	public boolean isSINK() {
+		return SINK;
+	}
+	public void setSINK(boolean sINK) {
+		SINK = sINK;
+	}
+	public boolean isSHOE_RACK() {
+		return SHOE_RACK;
+	}
+	public void setSHOE_RACK(boolean sHOE_RACK) {
+		SHOE_RACK = sHOE_RACK;
+	}
+	public boolean isREFRIGERATOR() {
+		return REFRIGERATOR;
+	}
+	public void setREFRIGERATOR(boolean rEFRIGERATOR) {
+		REFRIGERATOR = rEFRIGERATOR;
+	}
+	public boolean isBATH_TUBE() {
+		return BATH_TUBE;
+	}
+	public void setBATH_TUBE(boolean bATH_TUBE) {
+		BATH_TUBE = bATH_TUBE;
+	}
+}
