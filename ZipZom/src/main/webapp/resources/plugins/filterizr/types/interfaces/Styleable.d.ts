8b4bb1072df6613bfdf5a31418385f00fd2a28a4
jhy950210 <66720964+jhy950210@users.noreply.github.com>
2020-10-06 17:17:33 +0900

    sidebar/footer 공통부분 jsp

---
+++ ZipZom/src/main/webapp/resources/plugins/filterizr/types/interfaces/Styleable.d.ts
@@ -0,0 +1,5 @@
+import StyledFilterizrElement from './resources/StyledFilterizrElement';
+import StyledFilterizrElements from './resources/StyledFilterizrElements';
+export interface Styleable {
+    readonly styles: StyledFilterizrElement | StyledFilterizrElements;
+}
